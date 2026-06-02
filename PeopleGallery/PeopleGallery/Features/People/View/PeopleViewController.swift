eca3fbb145041dd84acc65c0e67670d96ba1675c
karlaemartins <karlaemartinss@gmail.com>
2026-06-02 15:37:35 -0300

    Setup people screen and collection view

--- PeopleGallery/PeopleGallery/Features/People/View/PeopleViewController.swift
+++ PeopleGallery/PeopleGallery/Features/People/View/PeopleViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 final class PeopleViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "People Gallery"
         view.backgroundColor = .systemBackground
+        
+        configureCollectionView()
+        
+    }
+    
+    private func configureCollectionView() {
+        
+        let layout = UICollectionViewFlowLayout()
+
+        layout.itemSize = CGSize(width: 140, height: 180)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor)])
+        
+        
         
     }
 }
